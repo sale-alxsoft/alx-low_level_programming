3432813826e142fe24350bb9a90a8ef6864cd5a7
Sale-alxsoft <sebsale7@gmail.com>
2022-03-10 20:49:59 +0000

    print the size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,5 +19,5 @@ int main(void)
 	printf("Size of a long int: %lu.\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu.\n", (unsigned long)sizeof(e));
-
+	return (0);
 }
