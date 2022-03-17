a3f0792186d7a042f6f5cda19da6902f8a53c581
Sale-alxsoft <sebsale7@gmail.com>
2022-03-17 20:55:53 +0000

    prints number between 0 and 10 including 0 but not 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 0 to 10
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
