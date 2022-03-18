cf599f6afcb9817c792166a3e95f1067a4d7b8c6
Sale-alxsoft <sebsale7@gmail.com>
2022-03-18 12:45:03 +0000

    print all possible combinations of digits separated by comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible cobinations of single-digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
