090b3091bfc5bcccdf2483962b3e099fd4f1ef52
Sale-alxsoft <sebsale7@gmail.com>
2022-03-19 20:41:01 +0000

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
