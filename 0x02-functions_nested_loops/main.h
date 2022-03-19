590541daeaa3c3d13bf8cd388d5d05ee53d4fce3
Sale-alxsoft <sebsale7@gmail.com>
2022-03-19 21:02:47 +0000

    functions

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,1 @@
-#include <unistd.h>
-/**
- * _putchar - writes the character c to stdout
- *
- *
- * Return: On succes
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char c);
