bc234d2f2a674ffd7b0486cdf0761bded5c46459
Sale-alxsoft <sebsale7@gmail.com>
2022-03-20 20:33:35 +0000

    computes the absolute value of a number

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,15 @@
 
 /**
  * _abs - computes the absolute value of an integer.
+ * @n: number
  * Return: Always 0.
  */
 
-int _abs(int)
+int _abs(int n)
 {
-	int i;
-
-	_putchar(abs(i));
-	return (0);
+	if (n > 0)
+	{
+		return (n);
+	}
+	return (-n);
 }
