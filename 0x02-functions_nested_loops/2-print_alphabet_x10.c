3d7f31a52e4540b3c0ce4bf6c3c778f3342f8d48
Sale-alxsoft <sebsale7@gmail.com>
2022-03-20 18:25:03 +0000

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ * Return: null
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char alpha;
+
+	i = 0;
+	alpha = 'a';
+
+	while (i < 10)
+	{
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
