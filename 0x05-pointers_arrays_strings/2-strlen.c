09e1d9343ca6e492617abe12082793b76c1fd515
Sale-alxsoft <sebsale7@gmail.com>
2022-03-21 19:38:10 +0000

    returns the length of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen(char *s) - returns the length of a string.
+ * @s: a pointer to an char
+ * Return: Alway 0.
+ */
+int _strlen(char *s)
+{
+	_putchar("%d", strlen(*s));
+	return (0);
+}
