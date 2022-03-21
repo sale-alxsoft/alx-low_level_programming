f442b09aaad8d681237595dbecf0533403d3f017
Sale-alxsoft <sebsale7@gmail.com>
2022-03-21 22:26:29 +0000

    reverses string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses a string
+ * @s: a pointer to a char
+ * Return: null
+ */
+void rev_string(char *s)
+{
+	int i;
+	
+	i = strlen(s);
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}
