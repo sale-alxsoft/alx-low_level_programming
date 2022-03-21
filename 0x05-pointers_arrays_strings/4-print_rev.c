238f7634a4f6de87738ad20743aa2f207e144557
Sale-alxsoft <sebsale7@gmail.com>
2022-03-21 21:10:42 +0000

    prints the reverse of the string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: a pointer to a char
+ * Return: null
+ */
+void print_rev(char *s)
+{
+	int i;
+	
+	i = 0;
+	while (*s++)
+	{
+		_putchar(*(s + i));
+		i++;
+	}
+	_putchar('\n');
+}
