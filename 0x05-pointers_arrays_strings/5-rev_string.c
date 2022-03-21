33aa2409836722d3cd5c327e814f03fbf3e098d2
Sale-alxsoft <sebsale7@gmail.com>
2022-03-21 23:07:29 +0000

    reverses string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,8 +15,11 @@ void rev_string(char *s)
 	i = strlen(s) - 1;
 	while (i >= 0)
 	{
-		_putchar(*(s + j));
-		_putchar(*(s + i));
+		char temp;
+
+		temp = *(s + i);
+		*(s + i) = *(s + j);
+		*(s + j) = temp;
 		i--;
 		j++;
 	}
