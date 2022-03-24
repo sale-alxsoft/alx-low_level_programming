d43a4d272b058089170c84e2b74e5f2916c1a4d7
sale-alxsoft <sebsale7@gmail.com>
2022-03-24 16:42:41 +0300

    concantanate strings and overwrite the terminating null byte at the end of the string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatenate strings
+ * @dest: a pointer to char
+ * @src: a pointer to char
+ * Return: dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char termn = '\0';
+
+	*dest = strcat(*dest, *src);
+	*dest = strcat(*dest, termn);
+	return (*dest);
+}
