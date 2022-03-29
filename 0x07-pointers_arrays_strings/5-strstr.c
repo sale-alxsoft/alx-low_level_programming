faa85543c57c1460eba156050d9d224cce951360
sale-alxsoft <sebsale7@gmail.com>
2022-03-29 17:19:25 +0300

    strstr - a function that locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: a string to be scanned
+ * @needle: substring to be searched
+ * Return: haystack or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	int i = 0;
+	int j = 0;
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[j])
+		{
+			_putchar("%c", s[i]);
+			j += 1;
+		}
+		i++;
+	}
+	return ('\0');
+}
