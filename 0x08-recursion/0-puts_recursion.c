92ff494e69747e0aa1cb1d742af7a9b19a6cf465
sale-alxsoft <sebsale7@gmail.com>
2022-03-31 03:25:51 +0300

    puts - a function that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,23 +1,16 @@
 #include "main.h"
+
 /**
- * _puts_recursion - Entry point
- *
- * @s: string
- *
- * Return: Always 0 (Success)
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
  */
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-	if (s[i] == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
-	if (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		_puts_recursion(&s[i + 1]);
-	}
+		_putchar(*s);
+		_puts_recursion(s + 1);
 }
