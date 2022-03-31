eccda43bb1a3a2448098f015f891b71d0f4ff7ab
sale-alxsoft <sebsale7@gmail.com>
2022-03-31 03:50:48 +0300

    pow - a function that returns the value of x raised to the power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _pow_recursion - Entry point
+ *
+ * @x: numero_1
+ *
+ * @y: numero a la potencia
+ *
+ * Return: Always 0 (Success)
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y > 0)
+	{
+		return (0 + x * _pow_recursion(x, y - 1));
+	}
+	return (y);
+}
