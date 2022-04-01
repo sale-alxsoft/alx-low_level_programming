8148f6bf8758928d97f0f1b91414576f3ec0e5a2
sale-alxsoft <sebsale7@gmail.com>
2022-04-01 15:44:36 +0300

    a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of excutable file
+ * @argc - count the number of arguments from command line
+ * @argv: a pointer to the strings that are passed from commad line
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
