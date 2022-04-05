11f7afa299f74595802255df756b5182b2d16509
sale-alxsoft <sebsale7@gmail.com>
2022-04-05 14:50:32 +0300

    ction that returns a pointer to a newly allocated space in memory, which contains a copy of the string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,21 +5,27 @@
  * _strdup - return the pointer to a the allocated space which
  * contains a copy of teh string given as a
  * @str: a pointer to a string
- * Return: NULL if str = NULL, a pointer to a duplicated string, NULL if the memory is insufficient
+ * Return: NULL if str = NULL, a pointer to a duplicated string,
+ * NULL if the memory is insufficient
  */
 char *_strdup(char *str)
 {
 	char *dupstr;
+	unsigned int i;
 
-	dupstr = malloc(sizeof(str));
+	dupstr = (char *)malloc(sizeof(str));
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	dupstr = str;
-	if (sizeof(dupstr) > sizeof(str))
+	if (dupstr == NULL)
 	{
 		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+		dupstr[i] = str[i];
+		i++;
 	}
 	return (dupstr);
 }
