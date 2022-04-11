ab2609cb3719d9b351fefbb8826738ad1f2cf4a0
sale-alxsoft <sebsale7@gmail.com>
2022-04-11 18:55:19 +0300

    user defined type dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef THIS
+#define THIS
+/**
+ * struct dog - create a new data type of dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
