602a6117973898255b34e726a69b112a5dfba0b0
sale-alxsoft <sebsale7@gmail.com>
2022-04-11 20:53:24 +0300

    it create user difined type dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,11 +6,14 @@
  * @age: second member
  * @owner: third member
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
