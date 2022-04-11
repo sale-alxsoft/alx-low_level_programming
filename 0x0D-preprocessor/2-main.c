789fb399d398bceae5c787c0f7638534c8a0ab5f
sale-alxsoft <sebsale7@gmail.com>
2022-04-11 15:15:09 +0300

    prints the name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar(__FILE__);
+	return (0);
+}
