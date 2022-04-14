b3b4fbef2ebf1f85462080df083e3483c507fadb
sale-alxsoft <sebsale7@gmail.com>
2022-04-14 04:56:42 +0300

    header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,6 @@
 #ifndef THIS
 #define THIS
 #include <stdlib.h>
-int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action(int)));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
