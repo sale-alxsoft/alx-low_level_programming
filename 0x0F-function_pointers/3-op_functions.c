ed5feb506256c7f252e5b59af1f23dcb8f908691
sale-alxsoft <sebsale7@gmail.com>
2022-04-14 19:20:59 +0300

    correction on betty style

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 /**
  * op_add - add a and b
- * op_sub - substract a and b
- * op_mul _ multiply a and b
- * op_div - divide a to b
- * op_mod - return the remainder
  * @a: first variable
  * @b: second variable
  * Return: the outcome of the operation
@@ -14,18 +11,43 @@ int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+ * op_sub - substract b from a
+ * @a: first variable
+ * @b: the second variable
+ * Return: the outcome of the operation
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+/**
+ * op_mul - multiply a and b
+ * @a: the first number
+ * @b: the second number
+ * Return: the product of a and b
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * op_div - divide a by b
+ * @a: the first number
+ * @b: the second number
+ * Return: outcome the operation
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+/**
+ * op_mod - calculate for the remainder of
+ * the division of the two numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the remainder
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
