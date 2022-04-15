a887e43bd0ea8d700843efe584b697383dffd688
sale-alxsoft <sebsale7@gmail.com>
2022-04-15 03:06:27 +0300

    add description for the function and it named arguments

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 
 /**
+ * print_numbers - print numbers
+ * @separator: separate the numbers
+ * @n: the number of arguments
+ * Return: NULL
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
@@ -9,13 +13,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	if (separator == NULL)
-		return;
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
-		if (i != (n - 1))
-			printf("%s ", separator);
+		if (separator != NULL)
+		{
+			if (i != (n - 1))
+				printf("%s", separator);
+		}
 	}
 	printf("\n");
+	va_end(args);
 }
