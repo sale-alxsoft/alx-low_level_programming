0a6c0a1e0ff4d6cd923ce603fe187ff61edd7a50
sale-alxsoft <sebsale7@gmail.com>
2022-04-16 16:42:42 +0300

    create struct all_type

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
 #include <stdarg.h>
+
+/**
+ * struct all_type - point to type and function of the arguments
+ * @spec: string point to type of arguments
+ * @f: function point to function that prints the argument
+ */
+typedef struct all_type
+{
+	char *spec;
+	void (*f)(va_list);
+} all_types;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
