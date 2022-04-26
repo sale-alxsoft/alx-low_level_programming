ab783ac3c6b31e1e690953308e2f9a6ace838c24
sale-alxsoft <sebsale7@gmail.com>
2022-04-26 11:58:08 +0300

    a function that return the number of nodes

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints all elements of linked list
+ * @h: points to the header
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
