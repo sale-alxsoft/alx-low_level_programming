1728957269c6cd2cf70df6ba1f41270f0c53477d
sale-alxsoft <sebsale7@gmail.com>
2022-05-02 15:29:48 +0300

    read textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints to stdout
+ * @filename: name of the file
+ * @letters: number of characters to be printed
+ *
+ * Return: number of characters print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buff = malloc(letters * sizeof(char));
+
+	fd = open(filename, O_RDONLY);
+	if (filename == NULL || fd == -1)
+	{
+		return (0);
+	}
+
+	read(fd, buff, letters);
+	buff[letters] = '\0';
+
+	close(fd);
+
+	printf("%s\n", buff);
+
+	fd = 0;
+	while (buff[fd] != '\0')
+	{
+		fd++;
+	}
+	return (fd);
+}
