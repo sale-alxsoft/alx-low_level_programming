661256ab611c503d67e267552f5c557fc6ffbaad
sale-alxsoft <sebsale7@gmail.com>
2022-05-03 20:25:29 +0300

    append text at the end of the file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,9 +20,9 @@ int append_text_to_file(const char *filename, char *text_content)
 			count++;
 	}
 
-	op = open(filename, O_RDWR | O_APPEND);
+	op = open(filename, O_WRONLY | O_APPEND);
 	wr = write(op, text_content, count);
-	if (op == -1 || wr == -1 || wr != op)
+	if (op == -1 || wr == -1)
 		return (-1);
 	close(op);
 	return (1);
