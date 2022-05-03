88718f783f9bd9fd2c0feccb1e5c761c5fbb28ff
sale-alxsoft <sebsale7@gmail.com>
2022-05-03 19:57:36 +0300

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * create_file - create a file and write it
+ * @filename: name of the file
+ * @text_content: content of the file
+ *
+ * Return: 1 on success, -1 if it fails
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int op;
+	int wr;
+	int count = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[count])
+		count++;
+	}
+
+	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	wr = write(op, text_content, count);
+	if (op == -1 | wr == -1 | wr != op)
+		return (-1);
+	close(op);
+	return (1);
+}
