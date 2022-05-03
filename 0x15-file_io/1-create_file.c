24472d362e181b969c176736175feaa689941784
sale-alxsoft <sebsale7@gmail.com>
2022-05-03 20:15:53 +0300

    a function that create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, count);
-	if ((op == -1) | (wr == -1) | (wr != op))
+	if (op == -1 || wr == -1)
 		return (-1);
 	close(op);
 	return (1);
